0480e39fb9d7571349c45289d2812bb480de009e
23 <529610588@qq.com>
2020-06-18 16:07:45 +0800

    验证权限

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -72,10 +72,13 @@
                                 pwd: this.ruleForm.pass
                             })
                         });
-                        // console.log(response);
+                         // console.log(response);
                         if (response.data) {
                             this.$alert("登录成功", "提示", {
                                 callback:()=>{
+                                    console.log(response.data.username+response.data.usergrade)
+                                    this.$store.state.username = response.data.username
+                                    this.$store.state.usergrade = response.data.usergrade
                                     this.$router.push('/')
                                 }
                             });
--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,16 @@ Vue.use(Vuex)
 export default new Vuex.Store({
 
   state: {
+    username:'',
+    usergrade:''
   },
   mutations: {
+    // updateCoursePage(state,name,grade) {
+    //   console.log(name);
+    //   console.log(grade);
+    //   state.username = name;
+    //   state.usergrade=grade;
+    // }
   },
   actions: {
   },
