c9efc518cb00bca652d1df7df2c20fea11cfcf4e
quanzhou88 <1334409562@qq.com>
2020-06-18 10:44:23 +0800

    quanzhou6185

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Experts from 'Experts'
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    Experts
   }
 })
